2a85bacd7581c35820651e8e2bec5a18ce92206b
Dante Gates <dantedgates@gmail.com>
2021-08-27 07:31:55 -0400

    learning shape of trends, scale is still off

--- stl.py
+++ stl.py
@@ -2,14 +2,26 @@ import numpy as np
 import tensorflow as tf
 
 
-class SeasonalEmbedding(tf.keras.layers.Layer):
+
+class SeasonalMixin:
+    @staticmethod
+    def fourier_series(a_n, b_n, n, period, t):
+        x = (2 * np.pi * tf.multiply(t, n)) / period
+        return (
+            tf.reduce_sum(tf.multiply(tf.cos(x), a_n), axis=1, keepdims=True)
+            + tf.reduce_sum(tf.multiply(tf.sin(x), b_n), axis=1, keepdims=True)
+        )
+
+
+
+class SeasonalEmbedding(tf.keras.layers.Layer, SeasonalMixin):
     def __init__(self, *args, input_dim, period, N, **kwargs):
         super().__init__(*args, **kwargs)
         self.input_dim = input_dim
         self.period = period
         self.N = N
         # model params
-        self.a_n, self.b_n = [tf.keras.layers.Embedding(input_dim=input_dim, output_dim=N, input_length=1)] * 2
+        self.a_n, self.b_n = [tf.keras.layers.Embedding(input_dim=input_dim, output_dim=N, input_length=1) for _ in range(2)]
 
 
     def call(self, input_tensor, training=False):
@@ -30,62 +42,58 @@ class SeasonalEmbedding(tf.keras.layers.Layer):
             self.period)
         
         return instance_trend + global_trend
-
-    @staticmethod
-    def fourier_series(a_n, b_n, t, n, period):
-        x = (2 * np.pi * tf.multiply(t, n)) / period
-        return (
-            tf.reduce_sum(tf.multiply(tf.cos(x), a_n), axis=1, keepdims=True)
-            + tf.reduce_sum(tf.multiply(tf.sin(x), b_n), axis=1, keepdims=True)
-        )
     
 
-class PooledSeasonalEmbedding(tf.keras.layers.Layer):
-    def __init__(self, *args, period, N, **kwargs):
+class CallaborativeSeasonalEmbedding(tf.keras.layers.Layer, SeasonalMixin):
+    def __init__(self, *args, period, N, n_layers, **kwargs):
         super().__init__(*args, **kwargs)
         self.period = period
         self.N = N
+        self.n_layers = n_layers
         # model params
-        self.a_n, self.b_n = [tf.keras.layers.Dense(self.N, activation='linear')] * 2
+        self.a_n = [tf.keras.layers.Dense(self.N, activation='linear')
+                    for _ in range(self.n_layers)]
+        self.b_n = [tf.keras.layers.Dense(self.N, activation='linear')
+                    for _ in range(self.n_layers)]
 
     def call(self, input_tensor, training=False):
         X, t = input_tensor
         n = (tf.range(self.N, dtype='float') + 1)[None, :]
 
+        a_n = b_n = X
+        for W_a, W_b in zip(self.a_n, self.b_n):
+            a_n, b_n = W_a(a_n), W_b(b_n)
+
         return self.fourier_series(
-            self.a_n(X),
-            self.b_n(X),
+            a_n,
+            b_n,
             n,
             self.period,
             t)
 
-    @staticmethod
-    def fourier_series(a_n, b_n, n, period, t):
-        x = (2 * np.pi * tf.multiply(t, n)) / period
-        return (
-            tf.reduce_sum(tf.multiply(tf.cos(x), a_n), axis=1, keepdims=True)
-            + tf.reduce_sum(tf.multiply(tf.sin(x), b_n), axis=1, keepdims=True)
-        )
 
-    
-class LinearTrendEmbedding(tf.keras.layers.Layer):
-    def __init__(self, *args, t_range, n_changepoints=25, checkpoint_range=.8, **kwargs):
+class CallaborativeLinearTrendEmbedding(tf.keras.layers.Layer):
+    def __init__(self, *args, output_dim, t_range, n_changepoints=25, checkpoint_range=.8, **kwargs):
         super().__init__(*args, **kwargs)
+        self.output_dim = output_dim
         self.t_range = t_range
         self.n_changepoints = n_changepoints
         self.checkpoint_range = checkpoint_range
-        self.r = tf.keras.layers.Reshape((3, self.n_changepoints))
-        self.δ = tf.keras.layers.Dense(self.n_changepoints * 3, kernel_regularizer='l1')
-        self.m = tf.keras.layers.Dense(1)
-        self.k = tf.keras.layers.Dense(1)
+        self.r = tf.keras.layers.Reshape((self.output_dim, self.n_changepoints))
+        self.δ = tf.keras.layers.Dense(self.n_changepoints * self.output_dim)
+        self.m = tf.keras.layers.Dense(self.output_dim)
+        self.k = tf.keras.layers.Dense(self.output_dim)
 
     def build(self, input_shape):
         self.s = tf.cast(tf.linspace(self.t_range[0], int(self.checkpoint_range * self.t_range[1]), self.n_changepoints + 1), 'float')[1:]
 
     def call(self, input_tensor, training=False):
         X, t = input_tensor
+
+        weights = self.r(self.δ(X))
+        self.add_loss(tf.keras.regularizers.l1()(weights))
         return self.trend(
-            self.r(self.δ(X)),
+            weights,
             self.m(X),
             self.k(X),
             self.s,
